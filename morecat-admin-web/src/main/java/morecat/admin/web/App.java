c19296228b004c4943089f0d422d14efeff5038d
emag <lanabe.lanabe@gmail.com>
2015-07-24 00:17:18 +0900

    Add basic layout

--- morecat-admin-web/src/main/java/morecat/admin/web/App.java
+++ morecat-admin-web/src/main/java/morecat/admin/web/App.java
@@ -22,11 +22,15 @@ public class App {
       new ClassLoaderAsset("index.html", App.class.getClassLoader()), "index.html");
     deployment.addAsWebResource(
       new ClassLoaderAsset("index.xhtml", App.class.getClassLoader()), "index.xhtml");
+    deployment.addAsWebResource(
+      new ClassLoaderAsset("entries/list.xhtml", App.class.getClassLoader()), "entries/list.xhtml");
 
     deployment.addAsWebInfResource(
       new ClassLoaderAsset("WEB-INF/web.xml", App.class.getClassLoader()), "web.xml");
     deployment.addAsWebInfResource(
       new ClassLoaderAsset("WEB-INF/template.xhtml", App.class.getClassLoader()), "template.xhtml");
+
+    deployment.addAllDependencies();
 
     container.start().deploy(deployment);
   }
