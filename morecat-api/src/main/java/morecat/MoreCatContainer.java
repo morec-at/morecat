fabc7eeb938bacc2855e5d94003241f4a1886268
emag <lanabe.lanabe@gmail.com>
2015-12-12 23:16:59 +0900

    Apply [SWARM-184] but leave [SWARM-201] problem

---
+++ morecat-api/src/main/java/morecat/MoreCatContainer.java
@@ -0,0 +1,42 @@
+package morecat;
+
+import org.wildfly.swarm.container.Container;
+import org.wildfly.swarm.datasources.DatasourcesFraction;
+import org.wildfly.swarm.jpa.JPAFraction;
+
+/**
+ * @author Yoshimasa Tanabe
+ */
+public class MoreCatContainer {
+
+  private static final String DB_HOST = MoreCatConfiguration.getDBHost();
+  private static final String DB_PORT = MoreCatConfiguration.getDBPort();
+  private static final String DB_USER = MoreCatConfiguration.getDBUser();
+  private static final String DB_PASSWORD = MoreCatConfiguration.getDBPassword();
+
+  public static Container newContainer() throws Exception {
+    Container container = new Container();
+
+    container.fraction(new DatasourcesFraction()
+      .jdbcDriver("org.postgresql", (d) -> {
+        d.driverDatasourceClassName("org.postgresql.Driver");
+        d.xaDatasourceClass("org.postgresql.xa.PGXADataSource");
+        d.driverModuleName("org.postgresql");
+      })
+      .dataSource("morecatDS", (ds) -> {
+        ds.driverName("org.postgresql");
+        ds.connectionUrl("jdbc:postgresql://" + DB_HOST + ":" + DB_PORT + "/morecat");
+        ds.userName(DB_USER);
+        ds.password(DB_PASSWORD);
+      })
+    );
+
+    container.fraction(new JPAFraction()
+      .inhibitDefaultDatasource()
+      .defaultDatasource("jboss/datasources/morecatDS")
+    );
+
+    return container;
+  }
+
+}
