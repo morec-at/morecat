659f92b15425849a76fd6f3c4d71027ed055dcca
emag <lanabe.lanabe@gmail.com>
2016-01-30 20:53:22 +0900

    Add admin entry apis

--- src/main/java/morecat/api/admin/EntryController.java
+++ src/main/java/morecat/api/admin/EntryController.java
@@ -1,17 +1,27 @@
 package morecat.api.admin;
 
 import morecat.MoreCatLogger;
+import morecat.api.admin.helper.MetaEntry;
+import morecat.api.admin.helper.MetaSingleEntry;
+import morecat.domain.Page;
+import morecat.domain.Pageable;
 import morecat.domain.model.Entry;
+import morecat.domain.model.EntryFormat;
+import morecat.domain.model.EntryState;
 import morecat.domain.service.EntryService;
 
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
+import javax.ws.rs.DefaultValue;
+import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
@@ -26,6 +36,51 @@ public class EntryController {
 
   @Inject
   private EntryService entryService;
+
+  // FIXME only admin can use this api.
+  @GET
+  @Produces(MediaType.APPLICATION_JSON)
+  public Page<MetaEntry> findAllByAdmin(@QueryParam("size") @DefaultValue("5") int size,
+                                    @QueryParam("page") @DefaultValue("0") int page) {
+    Page<Entry> allAuthorsEntries = entryService.findAllByAdmin(new Pageable(size, page));
+    return allAuthorsEntries.convert(MetaEntry.from(allAuthorsEntries.getElements()));
+  }
+
+  @GET
+  @Path("/{author}")
+  @Produces(MediaType.APPLICATION_JSON)
+  public Page<MetaEntry> findAllByAuthor(@PathParam("author") String authorName,
+                                     @QueryParam("size") @DefaultValue("5") int size,
+                                     @QueryParam("page") @DefaultValue("0") int page) {
+    Page<Entry> authorsEntries = entryService.findAllByAuthor(new Pageable(size, page), authorName);
+    return authorsEntries.convert(MetaEntry.from(authorsEntries.getElements()));
+  }
+
+  @GET
+  @Path("/{id:[0-9][0-9]*}")
+  @Produces(MediaType.APPLICATION_JSON)
+  public Response find(@PathParam("id") long id) {
+    Entry entry = entryService.find(id);
+    if (entry == null) {
+      return Response.status(Response.Status.NOT_FOUND).build();
+    }
+
+    return Response.ok(MetaSingleEntry.from(entry)).build();
+  }
+
+  @GET
+  @Path("/available-formats")
+  @Produces(MediaType.APPLICATION_JSON)
+  public EntryFormat[] getAvailableFormats() {
+    return EntryFormat.values();
+  }
+
+  @GET
+  @Path("/available-states")
+  @Produces(MediaType.APPLICATION_JSON)
+  public EntryState[] getAvailableStates() {
+    return EntryState.values();
+  }
 
   @POST
   @Consumes(MediaType.APPLICATION_JSON)
@@ -64,6 +119,7 @@ public class EntryController {
     present.setTitle(entry.getTitle());
     present.setContent(entry.getContent());
     present.setPermalink(entry.getPermalink());
+    present.setTags(entry.getTags());
     present.setState(entry.getState());
     present.setFormat(entry.getFormat());
 
---
+++ src/main/java/morecat/api/admin/helper/MetaEntry.java
@@ -0,0 +1,43 @@
+package morecat.api.admin.helper;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import morecat.domain.model.Entry;
+import morecat.domain.model.EntryState;
+import morecat.util.TimeUtils;
+
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+/**
+ * @author Yoshimasa Tanabe
+ */
+@Data
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+public class MetaEntry {
+
+  private Long id;
+  private String title;
+  private String authorName;
+  private String createdTime;
+  private Set<String> tags;
+  private EntryState state;
+
+  public static List<MetaEntry> from(List<Entry> entries) {
+    return entries.stream().map(MetaEntry::from).collect(Collectors.toList());
+  }
+
+  private static MetaEntry from(Entry entry) {
+    return new MetaEntry(
+      entry.getId(),
+      entry.getTitle(),
+      entry.getAuthorName(),
+      TimeUtils.composite(entry.getCreatedDate(), entry.getCreatedTime()).toString(),
+      entry.getTags(),
+      entry.getState()
+    );
+  }
+
+}
